543a334e845b4b0484931a4b79713009de7cf171
lifuyue <lif2@rose-hulman.edu>
2018-01-09 20:39:57 -0500

    p3 is finished

--- src/practice_problem3.py
+++ src/practice_problem3.py
@@ -32,10 +32,10 @@ import rosegraphics as rg
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    # run_test_practice_problem3a()
-    # run_test_practice_problem3b()
-    # run_test_practice_problem3c()
-    # run_test_practice_problem3d()
+    run_test_practice_problem3a()
+    run_test_practice_problem3b()
+    run_test_practice_problem3c()
+    run_test_practice_problem3d()
     run_test_practice_problem3e()
 
 
@@ -273,7 +273,7 @@ def practice_problem3b(sequence):
       :type: sequence: list    or tuple or string
     """
     ####################################################################
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPLEMENTATION REQUIREMENT:  You are NOT allowed to use the
@@ -288,8 +288,8 @@ def practice_problem3b(sequence):
     for k in range(0, len(sequence) - 1):
         if sequence[k] == sequence[-1]:
             return True
-        else:
-            return False
+    else:
+        return False
 
 
 def run_test_practice_problem3c():
@@ -453,36 +453,38 @@ def practice_problem3d(sequence):
       :type: sequence: list    or tuple or string
     """
     ####################################################################
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   8 minutes for each part of this problem.
     ####################################################################
-    # for k in range(len(sequence)):
-    #     if sequence[k] == 0:
-    #         return k
-    #     else:
-    #         return -1
-
+    for k in range(len(sequence)):
+        if sequence[k] == 0:
+            return k
+    else:
+        return -1
 
     ####################################################################
-    # TODO: 6. Just ABOVE this TODO, you should have implemented
+    # DONE: 6. Just ABOVE this TO DO, you should have implemented
     #     a solution for the   practice_problem3d   function.
     #     Here, put ANOTHER solution, as follows:
     #
-    #       -- Your FIRST solution (ABOVE this TODO)
+    #       -- Your FIRST solution (ABOVE this TO DO)
     #            should be a solution that IGNORES
     #              practice_problem3c (the previous problem).
     #
-    #       -- Your SECOND solution (BELOW this TODO)
+    #       -- Your SECOND solution (BELOW this TO DO)
     #            should be a solution that USES (calls)
     #              practice_problem3c.
     #
     #          This solution should *** HAVE NO LOOP (no FOR). ***
     ####################################################################
-    return practice_problem3c(sequence)[0]
+    if practice_problem3c(sequence):
+        return practice_problem3c(sequence)[0]
+    else:
+        return -1
 
 
 def run_test_practice_problem3e():
